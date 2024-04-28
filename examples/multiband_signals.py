75c1f9245da4a97b44c7248ccbe2fa143d0e41fe
umdetree <umdetree@gmail.com>
2024-04-28 14:59:25 +0800

    fix symbols non random bug

--- examples/multiband_signals.py
+++ examples/multiband_signals.py
@@ -4,6 +4,7 @@ use square root raised cosine pulse shaping to generate multiband signals
 
 import numpy as np
 import h5py
+import tqdm
 
 from ..filters import SqrtRaisedCosFilter
 from ..cs import multicoset
@@ -11,7 +12,7 @@ from ..cs import multicoset
 def main():
     qpsk_symbols = np.exp(1j * np.pi * np.array([1, 3, 7, 5]) / 4)
     data_len = 1000
-    n_symbs = 8
+    n_symbs = 400
     n_sigs = 6
     n_bands = 16
     bw = 50e6
@@ -28,17 +29,21 @@ def main():
 
     symbols = np.random.randint(0, len(qpsk_symbols), (data_len, n_symbs))
     symbols = qpsk_symbols[symbols]
+    r = np.random.uniform(0, 1, (data_len, n_sigs, n_symbs))
+    phi = np.exp(1j * np.random.uniform(0, 2 * np.pi, (data_len, n_sigs, n_symbs)))
+    symbols = r * phi
 
     sig_len = n_symbs * sps
-    signals = np.zeros((data_len, n_chs, int(sig_len / n_chs)), dtype=np.cdouble)
+    signals = np.zeros((data_len, int(sig_len / n_chs), 2 * n_chs), dtype=np.float32)
     occupied_bands = np.zeros((data_len, n_bands), dtype=np.uint8)
-    for i in range(data_len):
+    for i in tqdm.tqdm(range(data_len)):
         bids = np.random.choice(n_bands, n_sigs, replace=False)
         occupied_bands[i][bids] = 1
         signal = np.zeros(sig_len, dtype=np.cdouble)
-        for bid in bids:
+        for j, bid in enumerate(bids):
             fc = (bid + 0.5) * bw
-            signal += filter.pulse_shape(symbols[i], fc)
+            signal += filter.pulse_shape(symbols[i][j], fc)
+
 
         # multicoset sampling
         sub_samples, _ = multicoset.sample(
@@ -49,12 +54,17 @@ def main():
             n_chs,
             offsets=multicoset.DEFAULT_OFFSETS,
         )
-        signals[i] = sub_samples
+
+        sub_samples = sub_samples.T
+        signals[i, :, 0::2] = sub_samples.real
+        signals[i, :, 1::2] = sub_samples.imag
 
     # then save signals and occupied_bands
-    # with h5py.File("multiband.h5", "r") as file:
-    #     file.create_dataset("waveform", (data_len, sig_len), dtype=np.cdouble)
-    #     file.create_dataset("label", (data_len, n_bands), dtype=np.uint8)
+    with h5py.File("./pkt_detect/multiband.h5", "w") as file:
+        file.create_dataset("waveform", signals.shape, dtype=np.float32)
+        file.create_dataset("label", occupied_bands.shape, dtype=np.uint8)
+        file["waveform"][:] = signals
+        file["label"][:] = occupied_bands
 
 
 if __name__ == "__main__":
